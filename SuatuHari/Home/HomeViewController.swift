63b2fa95e9a2ca78243172824301b37e3e32fdbd
eldwinanthony <eldwinanthony@gmail.com>
2022-06-16 15:20:28 +0700

    add button to home

--- SuatuHari/Home/HomeViewController.swift
+++ SuatuHari/Home/HomeViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var circleViewChar: UIView!
+    @IBOutlet weak var charButton: UIButton!
+    @IBOutlet weak var checkpointButton: UIButton!
+    @IBOutlet weak var circleViewCheckpoint: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //set uiview to circlechar
+        circleViewChar.layer.cornerRadius = circleViewChar.layer.bounds.width / 2
+        circleViewChar.clipsToBounds = true
+        
+        //set radius and shadow to charbutton
+        charButton.layer.cornerRadius = 40
+        charButton.layer.shadowColor = UIColor.black.cgColor
+        charButton.layer.shadowOpacity = 0.4
+        charButton.layer.shadowOffset = .zero
+        charButton.layer.shadowRadius = 10
+        charButton.layer.shadowOffset = CGSize(width: 0.0, height: 9.0)
+        
+        //set uiview to circlecheckpoint
+        circleViewCheckpoint.layer.cornerRadius = circleViewChar.layer.bounds.width / 2
+        circleViewCheckpoint.clipsToBounds = true
+        
+        //set radius and shadow to checkpointbutton
+        checkpointButton.layer.cornerRadius = 40
+        checkpointButton.layer.shadowColor = UIColor.black.cgColor
+        checkpointButton.layer.shadowOpacity = 0.4
+        checkpointButton.layer.shadowOffset = .zero
+        checkpointButton.layer.shadowRadius = 10
+        checkpointButton.layer.shadowOffset = CGSize(width: 0.0, height: 9.0)
+        
+        
+        
     }
-
-
+    
 }
 
